3e16e9b2644f703c8bd4a52114eb9c7fd8d4a636
Batterex <163307175+Batterex@users.noreply.github.com>
2025-11-25 11:24:46 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const cors = require("cors");
 
+const Stripe = require("stripe");
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
